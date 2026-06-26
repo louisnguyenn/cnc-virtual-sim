96426a681e338d7a776e71aa62159b82c185b0bb
Louis Nguyen <louis.nguyen550@gmail.com>
2026-06-25 21:09:55 -0400

    parse each line and handle G or M word commands

--- src/parser/GcodeParser.cpp
+++ src/parser/GcodeParser.cpp
@@ -31,6 +31,7 @@ std::string GcodeParser::stripComment(const std::string &line)
 }
 
 /// @brief break gcode formatting "G1 X10.5 Y-3.2 F300" into {G:1, X:10.5, Y:-3.2, F:300}
+/// produces a map of the gcode to the value e.g. G -> 1
 /// @param line 
 /// @return {std::unordered_map} - words
 std::unordered_map<char, double> GcodeParser::tokenise(const std::string &line)
@@ -49,13 +50,14 @@ std::unordered_map<char, double> GcodeParser::tokenise(const std::string &line)
     return words;
 }
 
-/// @brief build 
+/// @brief build the parsed GCode into a C++ GCommand
+/// fills in the corresponding struct with the gcode
 /// @param gCode 
 /// @param words 
 /// @return {std::optional<GCommand>} 
 std::optional<GCommand> GcodeParser::buildCommand(int gCode, const std::unordered_map<char, double> &words)
 {
-    
+    // TODO
 }
 
 /// @brief parse a single line
@@ -63,7 +65,43 @@ std::optional<GCommand> GcodeParser::buildCommand(int gCode, const std::unordere
 /// @return {std::optional<GCommand>} line
 std::optional<GCommand> GcodeParser::parseLine(const std::string &line)
 {
+    std::string cleaned_line = stripComment(line);
 
+    // check if line is blank
+    if (cleaned_line.find_first_not_of(" \t\r\n") == std::string::npos)
+    {
+        return std::nullopt; // return empty optional
+    }
+
+    auto words = tokenise(cleaned_line);
+
+    if (words.empty())
+    {
+        return std::nullopt;
+    }
+
+    // if G word is found, handle motion command
+    if (words.count('G'))
+    {
+        int gCode = static_cast<int>(words.at('G'));
+
+        // TODO
+    }
+
+    // if M word is found, handle machine command
+    if (words.count('M'))
+    {
+        // TODO
+    }
+
+
+    return std::nullopt;
 } 
 
-std::vector<GCommand> parseFile(const std::string &path);   // parse an entire file
+/// @brief parse an entire file
+/// @param path 
+/// @return {std::vector<GCommand>} full vector of converted Gcode to C++ Gcommands
+std::vector<GCommand> parseFile(const std::string &path)
+{
+    // TODO
+}
