65c0b35f282928ea9904c0e6f6ef508601e80448
Louis Nguyen <louis.nguyen550@gmail.com>
2026-06-27 11:24:55 -0400

    overload subtraction and multiplication operators

--- include/motion/Vec3.h
+++ include/motion/Vec3.h
@@ -15,10 +15,16 @@ struct Vec3
     }
     
     // subtract two vectors
-    // TODO
+    Vec3 operator-(const Vec3 other) const
+    {
+        return {x - other.x, y - other.y, z - other.z};
+    }
 
     // multiply by a scalar
-    // TODO
+    Vec3 operator*(double scalar) const
+    {
+        return {x * scalar, y * scalar, z * scalar};
+    }
 
     // distance between two points
     // TODO
