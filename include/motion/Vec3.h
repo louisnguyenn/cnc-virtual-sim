231bc3c2c41242fcd7169eb75fb02dc1bfb76ad7
Louis Nguyen <louis.nguyen550@gmail.com>
2026-06-27 11:22:17 -0400

    add vector and machine state header files

---
+++ include/motion/Vec3.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <cmath>
+
+/// @brief vector operations
+struct Vec3
+{
+    double x{0.0};
+    double y{0.0};
+    double z{0.0};
+
+    // add two vectors
+    // TODO
+    
+    // subtract two vectors
+    // TODO
+
+    // multiply by a scalar
+    // TODO
+
+    // distance between two points
+    // TODO
+
+    // length of vector from origin
+    // TODO
+}
