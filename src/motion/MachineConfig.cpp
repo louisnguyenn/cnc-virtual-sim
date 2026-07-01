385470a90e7f14c8f54bc5612197f59f9007521a
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-01 11:41:18 -0400

    load config info to struct with json parsing

--- src/motion/MachineConfig.cpp
+++ src/motion/MachineConfig.cpp
@@ -5,4 +5,41 @@
 
 bool MachineConfig::loadFromFile(const std::string &path)
 {
+    // open file
+    std::ifstream file(path);
+    if (!file.is_open())
+    {
+        std::cerr << "Error: could not config_file file " << path << '\n';
+        return false;
+    }
+
+    // parse json file
+    nlohmann::json config_file;
+    try
+    {
+        file >> config_file;
+    }
+    catch (const nlohmann::json::parse_error &e)
+    {
+        std::cerr << "Error: failed to parse config_file: " << e.what() << '\n';
+        return false;
+    }
+
+    // load values from json into axis config and machine config structs
+    name = config_file["machine_name"];
+    x.minMm = config_file["axes"]["x"]["min_mm"];
+    x.maxMm = config_file["axes"]["x"]["max_mm"];
+    x.maxFeedrateMmpm = config_file["axes"]["x"]["max_feedrate_mmpm"];
+    y.minMm = config_file["axes"]["y"]["min_mm"];
+    y.maxMm = config_file["axes"]["y"]["max_mm"];
+    y.maxFeedrateMmpm = config_file["axes"]["y"]["max_feedrate_mmpm"];
+    z.minMm = config_file["axes"]["z"]["min_mm"];
+    z.maxMm = config_file["axes"]["z"]["max_mm"];
+    z.maxFeedrateMmpm = config_file["axes"]["z"]["max_feedrate_mmpm"];
+    spindleMinRpm = config_file["spindle"]["min_rpm"];
+    spindleMaxRpm = config_file["spindle"]["max_rpm"];
+    accelerationMmS2 = config_file["acceleration_mm_s2"];
+
+    std::cout << "[INFO] Loaded config: " << name << "\n";
+    return true;
 }
