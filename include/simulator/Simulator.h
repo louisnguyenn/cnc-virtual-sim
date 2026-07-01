566ecb7feb8e6eb829072a35dcfc598946916a15
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-01 17:28:28 -0400

    build skeleton for simulator header file

---
+++ include/simulator/Simulator.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "CommandQueue.h"
+#include "motion/MachineState.h"
+#include "motion/MachineConfig.h"
+#include "motion/MotionEngine.h"
+#include "parser/GcodeParser.h"
+#include "parser/GCommand.h"
+#include <string>
+#include <thread>
+#include <atomic>
+
+class Simulator
+{
+
+};
