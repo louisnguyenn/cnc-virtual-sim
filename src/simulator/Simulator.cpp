2f587b6d94c8eb372686cf61dd86899ccde57f48
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-01 17:59:37 -0400

    create function stubs

--- src/simulator/Simulator.cpp
+++ src/simulator/Simulator.cpp
@@ -22,3 +22,25 @@ const MachineState &Simulator::getState() const
 {
     return m_state;
 }
+
+/// @brief parser thread function
+/// @param path 
+/// @param queue 
+void Simulator::parserThread(const std::string &path, CommandQueue<GCommand> &queue)
+{
+
+}
+
+/// @brief simulator thread function
+/// @param queue 
+void Simulator::simulatorThread(CommandQueue<GCommand> &queue)
+{
+
+}
+
+/// @brief run a gcode file through the full pipeline
+/// @param gcodePath 
+void Simulator::run(const std::string &gcodePath)
+{
+
+}
