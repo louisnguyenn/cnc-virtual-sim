9e65c422ce13f9fb2c2d9f96d5929ade88404e35
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-04 11:40:35 -0400

    create function skeleton

---
+++ src/logger/Logger.cpp
@@ -0,0 +1,27 @@
+#include "logger/Logger.h"
+
+/// @brief constructor - store current time
+Logger::Logger() : m_startTime(std::chrono::steady_clock::now())
+{
+}
+
+void Logger::log(const Vec3 &pos, double feedrate, const std::string &status)
+{
+}
+
+// write all records to VSC file
+void Logger::writeToCSV(const std::string &path) const
+{
+}
+
+void Logger::printStatus(const Vec3 &pos, double feedrate, const std::string &status) const
+{
+}
+
+void Logger::printSummary(const MachineState &state) const
+{
+}
+
+int Logger::recordCount() const
+{
+}
