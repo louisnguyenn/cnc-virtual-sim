6f9041678c0147a0141eeb4fc89d2a6547f015e5
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-04 19:21:07 -0400

    implement level to string conversion function

--- src/logger/AppLogger.cpp
+++ src/logger/AppLogger.cpp
@@ -1,12 +1,35 @@
 #include "logger/AppLogger.h"
+#include <fstream>
+#include <iostream>
 
 // private
 void AppLogger::log(LogLevel level, const std::string &msg)
 {
+    if (level < m_level)
+    {
+        return;
+    }
+
+    std::cout << '[' << timestamp() << "] " << '[';
+    std::cout << levelToString(level);
+    std::cout << "] " << msg;
 }
 
 std::string AppLogger::levelToString(LogLevel level) const
 {
+    switch (level)
+    {
+    case LogLevel::DEBUG:
+        return "DEBUG";
+    case LogLevel::INFO:
+        return "INFO";
+    case LogLevel::WARN:
+        return "WARN";
+    case LogLevel::ERROR:
+        return "ERROR";
+    default:
+        return "UNKNOWN";
+    }
 }
 
 std::string AppLogger::timestamp() const
@@ -16,10 +39,14 @@ std::string AppLogger::timestamp() const
 // public
 void AppLogger::setLevel(LogLevel level)
 {
+    m_level = level;
 }
 
 void AppLogger::setFile(const std::string &path)
 {
+    std::ofstream file(path);
+
+    file.close();
 }
 
 void AppLogger::debug(const std::string &msg)
