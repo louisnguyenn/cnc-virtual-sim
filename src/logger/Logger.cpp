47ad9091c026519a3d4ed76ab288445fd8d60508
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-04 11:52:36 -0400

    implement writing function

--- src/logger/Logger.cpp
+++ src/logger/Logger.cpp
@@ -1,4 +1,6 @@
 #include "logger/Logger.h"
+#include <fstream>
+#include <iostream>
 
 /// @brief constructor - store current time
 Logger::Logger() : m_startTime(std::chrono::steady_clock::now())
@@ -16,6 +18,25 @@ void Logger::log(const Vec3 &pos, double feedrate, const std::string &status)
 // write all records to VSC file
 void Logger::writeToCSV(const std::string &path) const
 {
+    std::ofstream csv_file(path);
+
+    // write header row
+    csv_file << "time_ms,x,y,z,feedrate,status\n";
+
+    for (const auto &record : m_records)
+    {
+        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(record.timestamp - m_startTime).count();
+
+        csv_file << ms; // write timestamp
+
+        csv_file << record.pos.x << ',' << record.pos.y << ',' << record.pos.z; // write position
+
+        csv_file << record.feedrate; // write feedrate
+
+        csv_file << record.machineStatus << '\n'; // write machine status
+    }
+
+    csv_file.close();
 }
 
 void Logger::printStatus(const Vec3 &pos, double feedrate, const std::string &status) const
