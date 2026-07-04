9659e3dc3f6998a35f5dbce5927044b5c76c3734
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-04 11:42:31 -0400

    implement log function

--- src/logger/Logger.cpp
+++ src/logger/Logger.cpp
@@ -7,6 +7,10 @@ Logger::Logger() : m_startTime(std::chrono::steady_clock::now())
 
 void Logger::log(const Vec3 &pos, double feedrate, const std::string &status)
 {
+    Record record{std::chrono::steady_clock::now(), pos, feedrate, status};
+
+    // push onto records
+    m_records.push_back(record);
 }
 
 // write all records to VSC file
