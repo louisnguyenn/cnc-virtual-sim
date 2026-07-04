5a91456dd8be57bf2b1ea194a4ff2f98dbfb0ba7
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-04 19:09:56 -0400

    implement header

--- include/logger/AppLogger.h
+++ include/logger/AppLogger.h
@@ -1,13 +1,37 @@
 #pragma once
-#include <string>
 #include <fstream>
+#include <string>
 
 enum class LogLevel
 {
-
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR,
 };
 
 class AppLogger
 {
+  private:
+    LogLevel m_level{LogLevel::INFO};
+    std::ofstream m_file;
 
+    // private logger - singleton pattern (only one logger for the whole application)
+    AppLogger() = default;
+
+    void log(LogLevel level, const std::string &msg);
+    std::string levelToString(LogLevel level) const;
+    std::string timestamp() const;
+
+  public:
+    static AppLogger &get(); // singleton
+
+    // setters
+    void setLevel(LogLevel level);
+    void setFile(const std::string &path);
+
+    void debug(const std::string &msg);
+    void info(const std::string &msg);
+    void warn(const std::string &msg);
+    void error(const std::string &msg);
 };
