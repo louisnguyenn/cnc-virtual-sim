470fc3454e11aa18645b98e9b8ce9aafe39b9145
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-04 11:38:06 -0400

    create logger header

---
+++ include/logger/Logger.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "motion/MachineState.h"
+#include "motion/Vec3.h"
+#include <chrono>
+#include <string>
+#include <vector>
+
+class Logger
+{
+  public:
+    struct Record
+    {
+        std::chrono::steady_clock::time_point timestamp;
+        Vec3 pos;
+        double feedrate;
+        std::string machineStatus;
+    };
+
+  private:
+    std::vector<Record> m_records;
+    std::chrono::steady_clock::time_point m_startTime;
+
+  public:
+    // constructor
+    Logger();
+
+    // log every time the machine moves
+    void log(const Vec3 &pos, double feedrate, const std::string &status);
+
+    // write all records to VSC file
+    void writeToCSV(const std::string &path) const;
+
+    void printStatus(const Vec3 &pos, double feedrate, const std::string &status) const;
+
+    void printSummary(const MachineState &state) const;
+
+    int recordCount() const;
+};
