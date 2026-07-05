1c8395898a84040345c1a097b70eb86f8d23c9c4
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-05 11:09:38 -0400

    create skeleton

---
+++ src/simulator/AppConfig.cpp
@@ -0,0 +1,13 @@
+#include "simulator/AppConfig.h"
+
+// parse from argc/argv
+static AppConfig parse(int argc, char *argv[])
+{
+    
+}
+
+// print usage instructions
+static void printUsage()
+{
+
+}
