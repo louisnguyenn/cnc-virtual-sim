2c59d7d59d343ce005c7a731dfbc14db274d30e2
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-05 11:15:38 -0400

    begin implementation of parse function

--- src/simulator/AppConfig.cpp
+++ src/simulator/AppConfig.cpp
@@ -1,13 +1,45 @@
 #include "simulator/AppConfig.h"
+#include <iostream>
 
 // parse from argc/argv
 static AppConfig parse(int argc, char *argv[])
 {
-    
+    AppConfig app;
+
+    for (auto i{1}; i < argc - 1; ++i)
+    {
+        if (argv[i] == "--config")
+        {
+
+        }
+
+        if (argv[i] == "--input")
+        {
+        }
+
+        if (argv[i] == "--verbose")
+        {
+            app.verbose = true;
+        }
+
+        if (argv[i] == "--help")
+        {
+            printUsage();
+
+            app.valid = false;
+        }
+        else
+        {
+            std::cerr << "Error: unknown argument " << argv[i] << '\n';
+
+            app.valid = false;
+        }
+    }
+
+    return app;
 }
 
 // print usage instructions
 static void printUsage()
 {
-
 }
