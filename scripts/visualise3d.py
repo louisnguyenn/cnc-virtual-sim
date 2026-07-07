af8e23213b8301e2804b97a821304e857991f56d
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-07 16:32:34 -0400

    calculate total path length

---
+++ scripts/visualise3d.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("toolpath.csv")
+
+# print(df)
+
+# get points
+x = np.array(df["x"].values)
+y = np.array(df["y"].values)
+z = np.array(df["z"].values)
+
+# print(x)
+
+# calculate delta (change in) points
+dx = np.diff(x);
+dy = np.diff(y);
+dz = np.diff(z);
+
+distances = np.sqrt(dx**2 + dy**2 + dz**2)
+
+total_length = np.sum(distances)
+
+# print(total_length)
