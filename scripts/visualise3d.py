24f16c2c6ec7f5c4aa681a5de93e398c15cf4cca
Louis Nguyen <louis.nguyen550@gmail.com>
2026-07-07 16:35:37 -0400

    calculate average feedrate

--- scripts/visualise3d.py
+++ scripts/visualise3d.py
@@ -5,6 +5,9 @@ df = pd.read_csv("toolpath.csv")
 
 # print(df)
 
+#
+# calculate total path length
+#
 # get points
 x = np.array(df["x"].values)
 y = np.array(df["y"].values)
@@ -22,3 +25,13 @@ distances = np.sqrt(dx**2 + dy**2 + dz**2)
 total_length = np.sum(distances)
 
 # print(total_length)
+
+#
+# calculate average feed rate
+#
+
+feedrate = np.array(df["feedrate"].values)
+
+avg_feedrate = np.mean(feedrate)
+
+print(avg_feedrate)
